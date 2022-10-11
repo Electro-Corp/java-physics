6f6d1da2d6f32fe14cf9a90dccbb6292a4b83158
Androo <andrew.coon84@gmail.com>
2022-10-10 19:38:58 -0600

    del replit stuff

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-    deps = [
-        pkgs.nano
-        pkgs.graalvm17-ce
-        pkgs.maven
-        pkgs.replitPackages.jdt-language-server
-        pkgs.replitPackages.java-debug
-    ];
-}
